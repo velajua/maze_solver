d0aad000d16b5a8c697b5e7a70de2664b7df8179
Juan Velasquez <51091959+velajua@users.noreply.github.com>
2024-08-23 09:12:42 -0500

    Update maze_app.py

--- maze_app.py
+++ maze_app.py
@@ -159,8 +159,8 @@ which has {full}/{empty + full} numbers.
     instagram_user_id = secret.get('instagram_user_id')
 
     image_urls = [
-        "https://https://instagram-sudoku-deployer-4r64swfrtq-uc.a.run.app/sudoku_puzzle.png",
-        "https://https://instagram-sudoku-deployer-4r64swfrtq-uc.a.run.app/sudoku_solution.png"
+        "https://instagram-sudoku-deployer-4r64swfrtq-uc.a.run.app/sudoku_puzzle.png",
+        "https://instagram-sudoku-deployer-4r64swfrtq-uc.a.run.app/sudoku_solution.png"
     ]
     creation_ids = []
     for image_url in image_urls:
